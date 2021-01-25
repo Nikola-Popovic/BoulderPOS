1343f75866a354c39a9e24450f32d7c8a4ed36ca
Maigan Periot <maigan.periot@yahoo.ca>
2021-01-25 15:19:19 -0500

    renamed POS_Boulderers to BoulderPOS

--- BoulderPOS/Startup.cs
+++ BoulderPOS/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace POS_Boulderers
+namespace BoulderPOS
 {
     public class Startup
     {
