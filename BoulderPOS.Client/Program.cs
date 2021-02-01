75831497e58ed7210ce22ce914d3fbfd9a71c1bf
Maigan Periot <maigan.periot@yahoo.ca>
2021-02-01 08:42:42 -0500

    Renamed BoulderPOS to  BoulderPOS.Client

--- BoulderPOS.Client/Program.cs
+++ BoulderPOS.Client/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace BoulderPOS
+namespace BoulderPOS.Client
 {
     public class Program
     {
