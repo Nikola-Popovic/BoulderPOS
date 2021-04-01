8a20961bbc29ff73cac62a6fde8608e4570d397f
Nikola Popovic <popovic.k.nikola@gmail.com>
2021-04-01 12:14:52 -0400

    Client Page

--- BoulderPOS.Client/src/components/clientPreview/clientPreview.tsx
+++ BoulderPOS.Client/src/components/clientPreview/clientPreview.tsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react'
 import { Customer } from '../../data';
 import { useParams } from "react-router-dom";
 import { CustomerService } from '../../services/api';
+import { Button } from '@material-ui/core';
 import { Alert, AlertTitle } from '@material-ui/lab';
+import './clientPreview.css';
 
 export interface ClientPreviewProps {
 }
@@ -26,14 +28,52 @@ const ClientPreview : React.FunctionComponent<ClientPreviewProps> = (props) => {
         });
     }, [])
 
+    const handleCheckin = () => {
+
+    }
+
     return (
-        <div>
+        <div className="parent">
             {error && <Alert severity="error"> 
                 <AlertTitle> Error </AlertTitle>
                 An error occured while <strong> fetching the client </strong>
             </Alert> }
             {!error && 
-                <h2> {client?.firstName} {client?.lastName} </h2> 
+            
+                <div className="clientPanel" >
+                    <div className="pictureFrame"> Img </div>
+                    <div className="clientName"> 
+                        <h2> {client?.firstName} {client?.lastName} </h2>  
+                    </div>
+                    <div className="informationPanel">
+                        <div><strong>Email</strong>: {client?.email} </div>
+                        <div><strong>Birthdate</strong>: {client?.birthDate} </div>
+                    </div>
+                    <div className="informationPanel">
+                        <div><strong>Phone#</strong>: {client?.phoneNumber} </div>
+                    </div>
+                    <div className="subscriptionPanel"> 
+                        <div> Subscription: </div>
+                        {client?.subscription === null? <div> No subscription </div>
+                        : <div>
+                            <div>Start date: {client?.subscription.startDate}</div>
+                            <div>End date: {client?.subscription.endDate}</div>
+                        </div>
+                        }
+                    </div>
+                    <div className="entriesPanel"> 
+                        <div> Entries: </div>
+                        {client?.entries === null? <div> No entries </div>
+                        : <div>
+                            <div>Quantity: {client?.entries.quantity}</div>
+                            <div>Unlimited: {client?.entries.unlimitedEntries? "true" : "false"}</div>
+                        </div>
+                        }
+                    </div>
+                    <div className="buttonPanel">
+                        <Button onClick={() => handleCheckin()} color='primary' variant='contained'> Check-In </Button> 
+                    </div>
+                </div>
             }
         </div>
     )
