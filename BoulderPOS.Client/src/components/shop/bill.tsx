8036a0f60e508bfb9387f4e207a009ba0c07b56d
Nikola Popovic <popovic.k.nikola@gmail.com>
2021-04-06 11:25:35 -0500

    Link shop with Client

--- BoulderPOS.Client/src/components/shop/bill.tsx
+++ BoulderPOS.Client/src/components/shop/bill.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react'
+import React, {  useEffect, useState } from 'react'
 import { PaymentMethod } from '../../data/PaymentMethod';
-import { ProductInCart, toCurrency} from '../../data';
+import { Customer, ProductInCart, toCurrency} from '../../data';
 import './bill.css';
 
 const TPS = 0.05;
@@ -8,31 +8,64 @@ const TVQ = 0.09975 ;
 
 export interface BillProps {
     items: ProductInCart[], 
-    onPaymentConfirm: (method: PaymentMethod) => void
+    client: Customer | undefined,
+    onPaymentConfirm: (method: PaymentMethod) => void,
+    setBillRefresh : React.Dispatch<React.SetStateAction<boolean>>,
+    billRefresh : boolean
 }
 
 const Bill = (props : BillProps) => {
 
+    const [cart, setCart] = useState<ProductInCart[]>([]);
+    const [subTotal, setSubtotal] = useState<number>(0);
+
+    useEffect(() => {
+        setCart(props.items);
+        props.setBillRefresh(false);
+    }, [props.items, props.billRefresh]);
+
+    useEffect(() => {
+        setSubtotal(evaluateSubtotal());
+    }, [cart])
+
     const evaluateSubtotal = () =>{
-        const priceList = props.items.map(a => a.price * a.quantity)
+        const priceList = cart.map(a => a.price * a.quantity)
         return priceList.reduce((a, b) => a + b, 0)
     }
 
     const makeBillTable = () => {
-        return props.items.map((item) => <tr>
+        return cart.map((item) => <tr>
             <td>{item.name}</td>
             <td>{item.quantity}</td>
             <td>{toCurrency(item.price * item.quantity)}</td>
         </tr>)
     }
 
-    useEffect(() => {}, [props.items]);
-
     return <div className="billContext">
-                <h1 style={{display:"flex",justifyContent:"center"}}>Facture Courante</h1>
-                <p style={{display:"flex",justifyContent:"center"}}>PLACEHOLDER CLIENTPREVIEW</p>
+                <h1>Facture Courante</h1>
+                <div className='clientFrame'>
+                {
+                    props.client === undefined ? 
+                    <><button className="pictureFrame"> Add client </button>
+                       <div className="clientName"> 
+                            <h2> No client associated </h2>  
+                       </div>
+                    </> 
+                    : <> 
+                        <div className="pictureFrame"> Img </div>
+                        <div className="clientName"> 
+                            <h2> {props.client.firstName} {props.client.lastName} </h2>  
+                        </div>
+                        <div className="informationPanel">
+                            <div>Email : {props.client.email} </div>
+                            <div>Numéro de tél. : {props.client.phoneNumber} </div>
+                        </div>
+                      </>
+                }
+                </div>
+                
                 <div className="scrollable">
-                    <table style={{width:"90%",marginLeft: "auto", marginRight: "auto", marginTop: "50px"}}>
+                    <table className='billTable'>
                         <thead>
                             <tr>
                                 <th>Nom</th>
@@ -54,10 +87,10 @@ const Bill = (props : BillProps) => {
 
                     </div>
                     <div className="bottomSectionChild">
-                    <span >{toCurrency(evaluateSubtotal())} $</span>
-                    <span>{toCurrency(TPS * evaluateSubtotal())} $</span>
-                    <span>{toCurrency(TVQ * evaluateSubtotal())} $</span>
-                    <span className="billTotalSpan">{toCurrency(evaluateSubtotal() + TVQ * evaluateSubtotal()+ TPS * evaluateSubtotal())} $</span>
+                    <span >{toCurrency(subTotal)} $</span>
+                    <span>{toCurrency(TPS * subTotal)} $</span>
+                    <span>{toCurrency(TVQ * subTotal)} $</span>
+                    <span className="billTotalSpan">{toCurrency(subTotal + TVQ * subTotal+ TPS * subTotal)} $</span>
                     <button className="billPaymentButton" onClick={() => props.onPaymentConfirm(PaymentMethod.Cash)}>Payer</button>
 
                     </div>
--- BoulderPOS.Client/src/pages/shop.tsx
+++ BoulderPOS.Client/src/pages/shop.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useParams } from 'react-router';
 import { ProductsPanel, CategoriesPanel, Bill } from '../components/shop';
 import { PaymentMethod } from '../data/PaymentMethod';
 import { useSnackbar } from 'notistack';
-import { Product, ProductCategory, ProductInCart } from '../data';
-import { CategoryService } from '../services/api';
+import { Customer, Product, ProductCategory, ProductInCart } from '../data';
+import { CategoryService, CustomerService } from '../services/api';
 import "./shop.css";
 
 interface RouteParams {
@@ -19,11 +19,14 @@ const Shop = (props : ShopProps) => {
     const [categories, setCategories] = useState<ProductCategory[]>([]);
     const [products, setProducts] = useState<Product[]>([]);
     const [selectedCategory, setSelectedCategory] = useState<number>();
-    let { clientId } = useParams<RouteParams>();
     const [ cart, setCart ] = useState<ProductInCart[]>([]);
+    const [ billRefresh, setBillRefresh] = useState(false);
+    let { clientId } = useParams<RouteParams>();
+    const [client, setClient] = useState<Customer | undefined>();
     const { enqueueSnackbar } = useSnackbar();
 
     useEffect(() => {
+        // Categories
         let promise = CategoryService.getCategories();
         promise.then((response) => { 
             setCategories(response.data);
@@ -31,8 +34,17 @@ const Shop = (props : ShopProps) => {
         }).catch((error) => {
             enqueueSnackbar('Could not fetch categories', {variant:'error'})
             console.error(error);
-        })
+        });
     }, []);
+
+    useEffect(() => {
+        if (clientId === undefined) return;
+        let clientPromise = CustomerService.getCustomer(clientId);
+        clientPromise.then((reponse) => setClient(reponse.data))
+        .catch((error) => {
+            console.error(error);
+        });
+    }, [clientId])
 
     useEffect(() => {
         if (selectedCategory !== undefined) {
@@ -45,12 +57,10 @@ const Shop = (props : ShopProps) => {
         }
     }, [selectedCategory]);
 
-    /**
-     * 
-    useEffect(() => {
-    }, [cart])
+    useEffect( () => {
+        setBillRefresh(true);
+    }, [cart]);
     
-    */
     const handleShopProductClick = (product : Product) => {
         let tempCart = cart;
         if(tempCart.find(a => a.id == product.id) === undefined) {
@@ -83,7 +93,7 @@ const Shop = (props : ShopProps) => {
     }
     
     return <div className="shopPage">
-        <Bill items={cart} 
+        <Bill items={cart} client={client} setBillRefresh={setBillRefresh} billRefresh={billRefresh}
             onPaymentConfirm={(method: PaymentMethod) => onPaymentConfirm(method)}/>
         <div className="shopContext">
             <CategoriesPanel categories={categories} onCategoryClick={(categoryId) => setSelectedCategory(categoryId)}/>
