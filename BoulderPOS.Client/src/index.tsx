053d147d342107d3d60d21f457aed239ba7a3189
Nikola Popovic <popovic.k.nikola@gmail.com>
2021-04-06 11:25:09 -0500

    Remove unused package

--- BoulderPOS.Client/src/index.tsx
+++ BoulderPOS.Client/src/index.tsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons'
-
-library.add(fas);
 
 ReactDOM.render(
   <React.StrictMode>
